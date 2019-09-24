4a3aced97005597192b14b4017f0cd2ff6fd1374
Caio Oliveira <caiossoliveira@gmail.com>
2019-09-24 16:09:02 -0300

    fixed the typeMsg of each previous msg problem. Still have some issues to correct

--- fixEngineMD.c
+++ fixEngineMD.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char message[2500]; //variable shared by messageIdentifier and Pre-processing
+char fix_message[2500]; //variable shared by messageIdentifier and Pre-processing
+char message[2500];
 char message_type = ' '; //variable shared by messageIdentifier and Pre-processing. Used to know what type of message the array is.
+int count = 0;
+int next_message_length = 0;
 
 FILE* file; //variable shared by openFile and readNextCharacter
 
@@ -20,11 +23,11 @@ void preProcessingMDIncr();
 int main(){
 	openFile();
 	messageIdentifier();
-	preProcessing();
+	//preProcessing();
 }
 
 void openFile(){
-	file = fopen("fix.051.mixed-resumido.log", "r");
+	file = fopen("fix.051.mixed-simplified.log", "r");
 }
 
 char readNextCharacter(){
@@ -38,17 +41,110 @@ char readNextCharacter(){
 	}	
 }
 
-void clearMessage(int m){
+void carryMessage(int m){
 	/*this function intents to clean the message, removing the information not necessary 
 	for the pro-processing function. This way, the function removes the time*/
+	int i;
+	int msg_length = m;
+	int interval = 0;
+	next_message_length = 0;
 
 	while(message[m] != '' && m != 0){
-		message[m] = '\0';
+		next_message_length++;
 		m--;
+	}
+
+	for(i = 0; i < msg_length - next_message_length; i++){
+		fix_message[i] = message[i];
+		//message[i] = '\0'; //talvez
+	}
+
+	interval = msg_length - next_message_length;
+
+	for(i = 0; i < next_message_length + 1; i++){
+		message[i] = message[interval];
+		interval++;
+	}
+
+	printf("\n\n");
+	for(i=0; i < (msg_length - next_message_length) + 1; i++){
+		if(fix_message[i] == '')
+			printf("|");
+		else
+			printf("%c", fix_message[i]);
 	}
 }
 
 void messageIdentifier(){
+	int status_tag_35 = 0; //false
+	int status_eof = 0; //false
+	int m = 0;
+
+	for(int i=0; i<2500; i++){
+		message[i] = ' ';
+	}
+
+	while(!status_eof){ //
+		
+		char character = ' ';
+		m = next_message_length;
+
+		while(status_tag_35 < 2 && status_eof == 0){ //false //the status 35 has to be false and the end of file too
+			character = readNextCharacter();
+			if(character == '\0'){ //if character equals NULL
+				status_eof = 1; //true
+			}
+			else if(character == '3'){
+				message[m] = character;
+				m++;
+				character = readNextCharacter();
+				if(character == '5'){
+					message[m] = character;
+					m++;
+					character = readNextCharacter();
+					if(character == '='){
+						message[m] = character;
+						m++;
+						status_tag_35++; //true //If the 35 tag is detected the status var is changed. This way the while funcion will be false. This means that the message is complete
+						message_type = readNextCharacter(); //read the type of message
+						message[m] = message_type;
+						m++;
+					}
+				}
+				else{
+					message[m] = character;
+					m++;
+				}
+			}
+			else{
+				message[m] = character;
+				m++;
+			}
+		}
+		status_tag_35 = 1;
+		if(m > 0){
+			carryMessage(m);
+			/*printf("=======================================================\n");
+			for(int i=0; i<m; i++){
+				if(message[i] == '')
+					message[i] = '|';
+			}
+
+			preProcessing(); //send the message to the pre-processing function
+
+			for(int i=1; i<m; i++){ //to remove trash of the 35 tag
+				if(message[i] != '\0'){
+					printf("%c", message[i]);	
+				}
+				
+			}
+			printf("\n");
+			printf("========================================================\n");*/
+		}
+	}
+}
+
+void old_messageIdentifier(){
 	/*this functions detects each FIX message
 	  It is able to detect each 35 tag and this way to mount the message
 	*/
@@ -97,7 +193,7 @@ void messageIdentifier(){
 			}
 		}
 		if(m > 0){
-			clearMessage(m);
+			//clearMessage(m);
 			printf("=======================================================\n");
 			for(int i=0; i<m; i++){
 				if(message[i] == '')
