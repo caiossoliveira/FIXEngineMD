b666232551392fd5aef8ad4331539896adbab212
Caio Oliveira <caiossoliveira@gmail.com>
2019-09-25 11:56:08 -0300

    index problems about msg identification solved

--- fixEngineMD.c
+++ fixEngineMD.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char fix_message[2500]; //variable shared by messageIdentifier and Pre-processing
-char message[2500];
+char fix_message[5000]; //variable shared by messageIdentifier and Pre-processing
+char message[5000];
 char message_type = ' '; //variable shared by messageIdentifier and Pre-processing. Used to know what type of message the array is.
 int count = 0;
 int next_message_length = 0;
@@ -27,7 +27,7 @@ int main(){
 }
 
 void openFile(){
-	file = fopen("fix.051.mixed-simplified.log", "r");
+	file = fopen("fix.051.full_reflesh-simplified.log", "r");
 }
 
 char readNextCharacter(){
@@ -41,53 +41,63 @@ char readNextCharacter(){
 	}	
 }
 
-void carryMessage(int m){
-	/*this function intents to clean the message, removing the information not necessary 
-	for the pro-processing function. This way, the function removes the time*/
+void carryMessage(int msg_length){
+	/*this function identify each FIX message, removing the information not necessary 
+	for the pro-processing function.*/
+
 	int i;
-	int msg_length = m;
+	int msg_index = msg_length;
 	int interval = 0;
 	next_message_length = 0;
 
-	while(message[m] != '' && m != 0){
+	while(message[msg_index] != ''){
 		next_message_length++;
-		m--;
+		msg_index--;
 	}
 
-	for(i = 0; i < msg_length - next_message_length; i++){
+	for(i = 0; i < msg_length - next_message_length + 1; i++){ //+ 1 to take the SOH character
 		fix_message[i] = message[i];
-		//message[i] = '\0'; //talvez
 	}
 
-	interval = msg_length - next_message_length;
+	interval = msg_length - next_message_length + 1;
 
 	for(i = 0; i < next_message_length + 1; i++){
 		message[i] = message[interval];
 		interval++;
 	}
 
-	printf("\n\n");
-	for(i=0; i < (msg_length - next_message_length) + 1; i++){
+	printf("=======================================================\n");
+	for(int i=0; i<msg_index+1; i++){
 		if(fix_message[i] == '')
-			printf("|");
-		else
-			printf("%c", fix_message[i]);
+			fix_message[i] = '|';
 	}
+
+	preProcessing(); //send the message to the pre-processing function
+
+	for(int i=0; i < interval; i++){ //to remove trash of the 35 tag
+		if(fix_message[i] != '\0'){
+			printf("%c", fix_message[i]);	
+		}
+		
+	}
+	printf("\n");
+	printf("========================================================\n");
 }
 
 void messageIdentifier(){
 	int status_tag_35 = 0; //false
 	int status_eof = 0; //false
-	int m = 0;
+	int msg_index = 0;
+	int msg_length = 0;
 
-	for(int i=0; i<2500; i++){
+	for(int i=0; i<5000; i++){
 		message[i] = ' ';
 	}
 
 	while(!status_eof){ //
 		
 		char character = ' ';
-		m = next_message_length;
+		msg_index = next_message_length;
 
 		while(status_tag_35 < 2 && status_eof == 0){ //false //the status 35 has to be false and the end of file too
 			character = readNextCharacter();
@@ -95,121 +105,40 @@ void messageIdentifier(){
 				status_eof = 1; //true
 			}
 			else if(character == '3'){
-				message[m] = character;
-				m++;
+				message[msg_index] = character;
+				msg_index++;
 				character = readNextCharacter();
 				if(character == '5'){
-					message[m] = character;
-					m++;
+					message[msg_index] = character;
+					msg_index++;
 					character = readNextCharacter();
 					if(character == '='){
-						message[m] = character;
-						m++;
+						message[msg_index] = character;
+						msg_index++;
 						status_tag_35++; //true //If the 35 tag is detected the status var is changed. This way the while funcion will be false. This means that the message is complete
 						message_type = readNextCharacter(); //read the type of message
-						message[m] = message_type;
-						m++;
+						message[msg_index] = message_type;
+						msg_index++;
+					}
+					else{
+						message[msg_index] = character;
+						msg_index++;	
 					}
 				}
 				else{
-					message[m] = character;
-					m++;
+					message[msg_index] = character;
+					msg_index++;
 				}
 			}
 			else{
-				message[m] = character;
-				m++;
+				message[msg_index] = character;
+				msg_index++;
 			}
 		}
 		status_tag_35 = 1;
-		if(m > 0){
-			carryMessage(m);
-			/*printf("=======================================================\n");
-			for(int i=0; i<m; i++){
-				if(message[i] == '')
-					message[i] = '|';
-			}
-
-			preProcessing(); //send the message to the pre-processing function
-
-			for(int i=1; i<m; i++){ //to remove trash of the 35 tag
-				if(message[i] != '\0'){
-					printf("%c", message[i]);	
-				}
-				
-			}
-			printf("\n");
-			printf("========================================================\n");*/
-		}
-	}
-}
-
-void old_messageIdentifier(){
-	/*this functions detects each FIX message
-	  It is able to detect each 35 tag and this way to mount the message
-	*/
-
-	int status_tag_35 = 0; //false
-	int status_eof = 0; //false
-
-
-	for(int i=0; i<2500; i++){
-		message[i] = ' ';
-	}
-
-	while(!status_eof){ //
-		int m = 0;
-		char character = ' ';
-
-		status_tag_35 = 0; //false
-
-		while(status_tag_35 == 0 && status_eof == 0){ //false //the status 35 has to be false and the end of file too
-			character = readNextCharacter();
-			if(character == '\0'){ //if character equals NULL
-				status_tag_35 = 1; //true
-				status_eof = 1; //true
-			}
-			if(character == '3'){
-				message[m] = character;
-				m++;
-				character = readNextCharacter();
-				if(character == '5'){
-					message[m] = character;
-					m++;
-					character = readNextCharacter();
-					if(character == '='){
-						status_tag_35 = 1; //true //If the 35 tag is detected the status var is changed. This way the while funcion will be false. This means that the message is complete
-						message_type = readNextCharacter(); //read the type of message
-					}
-				}
-				else{
-					message[m] = character;
-					m++;
-				}
-			}
-			else{
-				message[m] = character;
-				m++;
-			}
-		}
-		if(m > 0){
-			//clearMessage(m);
-			printf("=======================================================\n");
-			for(int i=0; i<m; i++){
-				if(message[i] == '')
-					message[i] = '|';
-			}
-
-			preProcessing(); //send the message to the pre-processing function
-
-			for(int i=1; i<m; i++){ //to remove trash of the 35 tag
-				if(message[i] != '\0'){
-					printf("%c", message[i]);	
-				}
-				
-			}
-			printf("\n");
-			printf("========================================================\n");
+		if(msg_index > 0){
+			msg_length = msg_index;
+			carryMessage(msg_length);
 		}
 	}
 }
